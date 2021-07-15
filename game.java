6b047a0bc7aa33bbce48255436ed569be5aa7be0
Aso2001022 <83050765+Aso2001022@users.noreply.github.com>
2021-07-15 10:55:26 +0900

    Add files via upload

---
+++ game.java
@@ -0,0 +1,10 @@
+package pkgsaisyu;
+
+public class game {
+    public static void dispT(){
+        System.out.println("***********************");
+        System.out.println("       戦艦ゲーム       ");
+        System.out.println("***********************");
+    }
+
+}
